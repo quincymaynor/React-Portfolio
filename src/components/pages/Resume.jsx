fd832c2f1275816abc3d8a275f54eaca652c393d
Quincy Maynor <quincymaynor@gmail.com>
2023-10-02 21:48:31 -0500

    tweaked CSS

--- src/components/pages/Resume.jsx
+++ src/components/pages/Resume.jsx
@@ -1,8 +1,8 @@
 export default function Resume() {
   return (
-    <div>
+    <div class="resume" id="resume">
       <h1>Resume Page</h1>
-      <p>
+      <p class="resume-content" id="resume-content">
         My Resume will be here
       </p>
     </div>
